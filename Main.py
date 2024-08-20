8aaa2bd673686ebb336d3c8ab22a4c92f8602d63
flesher <56498635+flesher-dizainer@users.noreply.github.com>
2024-08-20 20:09:54 +0300

    add game statistic

--- Main.py
+++ Main.py
@@ -5,8 +5,11 @@ import json
 
 # загружаем список городов из json файла
 def get_cities_list_from_json(file_name):
-    with open(file_name, 'r', encoding='cp1251') as file:
-        return json.load(file)
+    try:
+        with open(file_name, 'r', encoding='cp1251') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return ''
 
 
 # сохраняем список городов в json
@@ -53,6 +56,36 @@ def check_city_name(city_name, cities_set):
     return city_name.lower() in cities_name_list
 
 
+# загрузка списка результатов игр в json
+def load_result_game_json():
+    try:
+        with open('result_game.json', 'r', encoding='cp1251') as file:
+            return json.load(file)
+    except:
+        return None
+
+
+# сохранение результатов игры в json файл
+def save_result_game_json(result_game: dict):
+    list_result_game = []
+    try:
+        with open('result_game.json', 'r+', encoding='cp1251') as file:
+            try:
+                list_result_game = json.load(file)
+            except:
+                list_result_game = []
+            if len(list_result_game) >= 5:
+                list_result_game.pop(0)
+    except FileNotFoundError:
+        pass
+    list_result_game.append(result_game)
+    with open('result_game.json', 'w+', encoding='cp1251') as file:
+        try:
+            json.dump(list_result_game, file, ensure_ascii=False, indent=4)
+        except:
+            print('Error save game result')
+
+
 def main():
     if path.exists('cities.json'):
         cities_list_json = get_cities_list_from_json('cities.json')
@@ -63,6 +96,13 @@ def main():
         save_dict_to_json('cities.json', cities_list)
     # удаляем названия городов, заканчивающиеся на плохую букву
     cities_set = del_bad_cities_name(cities_set)
+    # загружаем прошлую статистику игр
+    statistic_game_json = load_result_game_json()
+    if statistic_game_json == None:
+        print('Игр ещё небыло или статистика не найдена')
+    else:
+        for game_result in statistic_game_json:
+            print(game_result)
     start = True
     char_city_name = ''
     user_id = 1
@@ -92,9 +132,9 @@ def main():
         char_city_name = input_user_city_name[-1].upper()
         user_id = user_id ^ 1
     if user_id == 1:
-        print('Проиграл игрок')
+        save_result_game_json([{'user': '0', 'machine': '1'}])
     else:
-        print('Проиграла машина')
+        save_result_game_json([{'user': '1', 'machine': '0'}])
 
 
 if __name__ == "__main__":
